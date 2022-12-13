5a6bb5448e422e229712b150045ef0b48c2f90a7
aldaces <shadarsi@gmail.com>
2022-12-13 14:05:24 +0200

    add accordion

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,6 @@
 // // Import vendor jQuery plugin example
 // import '~/app/libs/mmenu/dist/mmenu.js'
+import '../../node_modules/bootstrap/js/src/collapse.js'
 
 document.addEventListener('DOMContentLoaded', () => {
 
